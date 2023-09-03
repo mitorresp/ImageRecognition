131b2f97d378d032c484412324cd7ce1cce68cf6
Miguel T <migueltp.oficial@gmail.com>
2023-09-03 06:51:50 +0200

    Add images

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import win32api, win32con
 
 BASE_IMG = "assets/img/"
 GAME_ZONE = [1400, 300, 650, 520]
-SPEED_LIST = [1, 0.7, 0.5, 0, 3]
+SPEED_LIST = [0.5, 0.3, 0.2, 0.2, 0.2, 0.2, 0.2]
 points = 0
+prev_shoots = []
 
 
 def lookForImage(imgName):
@@ -23,25 +24,41 @@ def lookForImage(imgName):
 def lookForInvaders():
     return [
         lookForImage("weapon_transparent"),
-        lookForImage("invader_01"),
-        lookForImage("invader_02"),
-        lookForImage("invader_03"),
         lookForImage("invader_04_transparent"),
+        lookForImage("invader_03"),
+        lookForImage("invader_02"),
+        lookForImage("invader_01"),
     ]
 
 
 def shootAt(box):
-    win32api.SetCursorPos((box.left + int(box.width / 2), 50 + box.top + box.height))
-    time.sleep(0.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    points = points + 5
-    print(
-        "\t\tShoted:" + str(box.left + int(box.width / 2)) + ", ",
-        str(box.top + int(box.height / 2)),
-        "  POINTS: ",
-        points,
-    )
+    global points, prev_shoots
+    _continue = True
+
+    for shot in prev_shoots[-3:]:
+        calc = shot - ((box.left + int(box.width / 2)))
+        if -5 >= calc <= 5:
+            _continue = False
+            print("calc", calc)
+
+    if _continue:
+        win32api.SetCursorPos(
+            (box.left + int(box.width / 2), 50 + box.top + box.height)
+        )
+        time.sleep(0.05)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+        prev_shoots.append(box.left + int(box.width / 2))
+        points += 5
+        print(
+            "\t\tShoted:" + str(box.left + int(box.width / 2)) + ", ",
+            str(box.top + int(box.height / 2)),
+            "  POINTS: ",
+            points,
+            prev_shoots,
+        )
+    if len(prev_shoots) > 5:
+        prev_shoots = prev_shoots[-3:]
 
 
 def stickman():
@@ -49,27 +66,29 @@ def stickman():
         search = lookForInvaders()
         found = False
 
-        idx = 0
+        idx = 5
         for x in search:
             if x != None:
                 print(
-                    (" invader 0" + str(idx) if idx >= 1 else " weapon ") + " -",
+                    (" invader 0" + str(idx) if idx <= 4 else " weapon ") + " -",
                     x,
                 )
                 time.sleep(0.01)
                 shootAt(x)
                 found = True
-            idx += 1
+            idx -= 1
 
+        global points
         if found:
             print("\n")
-        time.sleep(SPEED_LIST[0])
+            print("speed " + str(int(points / 250)))
+        time.sleep(SPEED_LIST[int(points / 250)])
 
 
 if __name__ == "__main__":
     print("Good Night Miguel.\n")
     time.sleep(2)
-    print("Starting\n")
+    print("Ready and Waiting...\n")
     time.sleep(1)
     stickman()
     print("\nBye bye, Miguel.")
